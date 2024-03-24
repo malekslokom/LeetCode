e4c4660872ccbecf286dc9b6765103964f8cb722
malekslokom <slokom.malek@gmail.com>
2024-03-24 09:30:33 +0100

    list and set

---
+++ Find the Duplicate Number.py
@@ -0,0 +1,44 @@
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+# There is only one repeated number in nums, return this repeated number.
+
+# You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,4,2,2]
+# Output: 2
+# Example 2:
+
+# Input: nums = [3,1,3,4,2]
+# Output: 3
+# Example 3:
+
+# Input: nums = [3,3,3,3,3]
+# Output: 3
+ 
+
+# Constraints:
+
+# 1 <= n <= 105
+# nums.length == n + 1
+# 1 <= nums[i] <= n
+# All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen=set()
+        for i in nums:
+            if i in seen:
+                return i
+            seen.add(i)
+        
